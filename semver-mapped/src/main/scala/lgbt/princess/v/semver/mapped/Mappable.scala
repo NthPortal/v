617eea4c3f57acf128ada5053d2a2e971c82c133
NthPortal <nthportal@gmail.com>
2021-09-12 12:04:47 -0400

    [wip] variance!!

--- semver-mapped/src/main/scala/lgbt/princess/v/semver/mapped/Mappable.scala
+++ semver-mapped/src/main/scala/lgbt/princess/v/semver/mapped/Mappable.scala
@@ -1,9 +1,16 @@
 package lgbt.princess.v.semver.mapped
 
 import lgbt.princess.v.semver.Identifiers
-import lgbt.princess.v.semver.Identifiers._
 
-trait Mappable[A, I <: Identifiers] {
+// Can't be `+A` because, while that does return a valid type, the compiler
+//   may select an implicit instance for a subtype of `A` that only maps a
+//   subset of valid values for `A`. It's especially problematic because
+//   `Nothing` is a subtype of `A` and maps an empty subset of valid values
+//   for `A`, and we need an instance for `Nothing` for `Mapping`s without one
+//   of the two types of `Identifiers`. Additionally, it causes ambiguous implicits.
+// Can be `-I` to support instances that can map from arbitrary `Identifiers`
+//   rather than only from one of the two subtypes of `Identifiers`.
+trait Mappable[A, -I <: Identifiers] {
   def map(identifiers: I): Option[A]
 
   /**
@@ -21,7 +28,7 @@ object Mappable {
    * A [[Mappable]] that forwards the implementation of
    * [[Mappable.unsafeMap() `unsafeMap`]] to [[Mappable.map() `map`]].
    */
-  trait ForwardingUnsafeMap[A, I <: Identifiers] extends Mappable[A, I] {
+  trait ForwardingUnsafeMap[A, -I <: Identifiers] extends Mappable[A, I] {
     /** A description of the result type of the mapping operation. */
     protected[this] def resultType: String
 
@@ -30,12 +37,9 @@ object Mappable {
         .getOrElse(throw new UnsupportedOperationException(s"cannot map '$identifiers' to $resultType"))
   }
 
-  private final class NothingIsMappable[I <: Identifiers] extends Mappable[Nothing, I] {
-    def map(identifiers: I): Option[Nothing] = None
-    def unsafeMap(identifiers: I): Nothing =
+  implicit object NothingIsMappable extends Mappable[Nothing, Identifiers] {
+    def map(identifiers: Identifiers): Option[Nothing] = None
+    def unsafeMap(identifiers: Identifiers): Nothing =
       throw new UnsupportedOperationException("cannot map identifiers to nothing")
   }
-
-  implicit val nothingIsMappableToPreRelease: Mappable[Nothing, PreRelease] = new NothingIsMappable[PreRelease]
-  implicit val nothingIsMappableToBuild: Mappable[Nothing, Build]           = new NothingIsMappable[Build]
 }
--- semver-mapped/src/main/scala/lgbt/princess/v/semver/mapped/Unmappable.scala
+++ semver-mapped/src/main/scala/lgbt/princess/v/semver/mapped/Unmappable.scala
@@ -3,7 +3,13 @@ package mapped
 
 import lgbt.princess.v.semver.Identifiers._
 
-trait Unmappable[A, I <: Identifiers] {
+// Can be `-A` because, unlike `Mappable`, an instance of this trait guarantees
+//   the ability to unmap all values of type `A`, so being able to unmap all
+//   values of a supertype of `A` is fine too.
+// Can be `+I`, but it's not useful because we never require implicit
+//   instances for the more general `Identifiers` (only for its two subtypes),
+//   and even if we did, it could cause ambiguous implicits.
+trait Unmappable[-A, I <: Identifiers] {
   def unmap(a: A): I
 }
 
